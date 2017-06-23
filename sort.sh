12052ffb695ca678d1324ff735d9a54e92b4f449
marcushuangzhaoyi <1002423907@qq.com>
2017-06-23 12:33:39 +0800

    Add files via upload

---
+++ sort.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# usage example: ./sort.sh
+
+#feel free to change Jan, Feb... to 1, 2.....
+
+MONTHS=(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
+for month in "${MONTHS[@]}"
+do
+	for item in `find ./ -type f -name "*$month*"`
+	do
+	if [ ! -d "./$month" ]; then
+  	mkdir ./"$month"
+	fi
+	mv $item ./$month
+	done
+	done
